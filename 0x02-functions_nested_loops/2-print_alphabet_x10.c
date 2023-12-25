45340f3bcd61693143906e1c339404530bf507ed
Izoukha <izoukhan@gmail.com>
2023-12-25 12:25:49 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i, j;
+
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = 'a';
+
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+	}
+}
