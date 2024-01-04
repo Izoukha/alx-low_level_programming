f8bc9e10636dfc9b9202a39e5aae8cf7ffb5590f
Izoukha <izoukhan@gmail.com>
2024-01-03 16:28:45 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,10 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-			{
-			_putchar(str[i]);
-			i += 2;
-			}
+	for (int i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
